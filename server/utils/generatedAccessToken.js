0449b839fc1e2e72198a64851482faaebd85d62b
shahriar2880 <shahriarreza436@gmail.com>
2025-02-10 04:09:47 +0600

    clodinary

--- server/utils/generatedAccessToken.js
+++ server/utils/generatedAccessToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 const generatedAccessToken = async(userId)=>{
     const token = await jwt.sign({ id : userId},
         process.env.SECRET_KEY_ACCESS_TOKEN,
-        { expiresIn : '5h'}
+        { expiresIn : '7d'}
     )
 
     return token
