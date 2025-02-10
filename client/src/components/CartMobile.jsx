2aa02ef566ce958d23b61e37d05cd1e385c310ae
shahriar2880 <shahriarreza436@gmail.com>
2025-02-10 22:04:55 +0600

    update client

--- client/src/components/CartMobile.jsx
+++ client/src/components/CartMobile.jsx
@@ -1,42 +1,70 @@
 import React from 'react'
-import { useGlobalContext } from '../provider/GlobalProvider'
-import { FaCartShopping } from 'react-icons/fa6'
 import { DisplayPriceInRupees } from '../utils/DisplayPriceInRupees'
 import { Link } from 'react-router-dom'
-import { FaCaretRight } from "react-icons/fa";
-import { useSelector } from 'react-redux'
+import { valideURLConvert } from '../utils/valideURLConvert'
+import { pricewithDiscount } from '../utils/PriceWithDiscount'
+import SummaryApi from '../common/SummaryApi'
+import AxiosToastError from '../utils/AxiosToastError'
+import Axios from '../utils/Axios'
+import toast from 'react-hot-toast'
+import { useState } from 'react'
+import { useGlobalContext } from '../provider/GlobalProvider'
+import AddToCartButton from './AddToCartButton'
 
-const CartMobileLink = () => {
-    const { totalPrice, totalQty } = useGlobalContext()
-    const cartItem = useSelector(state => state.cartItem.cart)
-
+const CardProduct = ({data}) => {
+    const url = `/product/${valideURLConvert(data.name)}-${data._id}`
+    const [loading,setLoading] = useState(false)
+  
   return (
-    <>
-        {
-            cartItem[0] && (
-            <div className='sticky bottom-4 p-2'>
-            <div className='bg-green-600 px-2 py-1 rounded text-neutral-100 text-sm  flex items-center justify-between gap-3 lg:hidden'>
-                    <div className='flex items-center gap-2'>
-                        <div className='p-2 bg-green-500 rounded w-fit'>
-                            <FaCartShopping/>
-                        </div>
-                        <div className='text-xs'>
-                                <p>{totalQty} items</p>
-                                <p>{DisplayPriceInRupees(totalPrice)}</p>
-                        </div>
-                    </div>
+    <Link to={url} className='border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded cursor-pointer bg-white' >
+      <div className='min-h-20 w-full max-h-24 lg:max-h-32 rounded overflow-hidden'>
+            <img 
+                src={data.image[0]}
+                className='w-full h-full object-scale-down lg:scale-125'
+            />
+      </div>
+      <div className='flex items-center gap-1'>
+        <div className='rounded text-xs w-fit p-[1px] px-2 text-green-600 bg-green-50'>
+              10 min 
+        </div>
+        <div>
+            {
+              Boolean(data.discount) && (
+                <p className='text-green-600 bg-green-100 px-2 w-fit text-xs rounded-full'>{data.discount}% discount</p>
+              )
+            }
+        </div>
+      </div>
+      <div className='px-2 lg:px-0 font-medium text-ellipsis text-sm lg:text-base line-clamp-2'>
+        {data.name}
+      </div>
+      <div className='w-fit gap-1 px-2 lg:px-0 text-sm lg:text-base'>
+        {data.unit} 
+        
+      </div>
 
-                    <Link to={"/cart"} className='flex items-center gap-1'>
-                        <span className='text-sm'>View Cart</span>
-                        <FaCaretRight/>
-                    </Link>
-                </div>
-            </div>
+      <div className='px-2 lg:px-0 flex items-center justify-between gap-1 lg:gap-3 text-sm lg:text-base'>
+        <div className='flex items-center gap-1'>
+          <div className='font-semibold'>
+              {DisplayPriceInRupees(pricewithDiscount(data.price,data.discount))} 
+          </div>
+          
+          
+        </div>
+        <div className=''>
+          {
+            data.stock == 0 ? (
+              <p className='text-red-500 text-sm text-center'>Out of stock</p>
+            ) : (
+              <AddToCartButton data={data} />
             )
-        }
-    </>
-    
+          }
+            
+        </div>
+      </div>
+
+    </Link>
   )
 }
 
-export default CartMobileLink
+export default CardProduct
