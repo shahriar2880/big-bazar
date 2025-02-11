1ebd6cac3d89e520efda1776d57ceda962350611
shahriar2880 <shahriarreza436@gmail.com>
2025-02-11 19:06:07 +0600

    search component

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import logo from "../assets/big-bazar.png"
+import Search from "./Search";
 
 const Header = () => {
   return (
     //header tag use for seo frendly
     <header className="h-16 shadow-md sticky top-0">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex items-center h-full px-2 justify-between">
         {/* logo */}
-        <div>
-            <div>
-                <img src={logo} alt='' height={50} width={80}/>
+        <div className="h-full">
+            <div className="h-full flex justify-center items-center">
+                <img src={logo} alt='' height={50} width={80} className="hidden lg:block"/>
+                <img src={logo} alt='' height={50} width={80} className="lg:hidden"/>
             </div>
         </div>
         {/* logo */}
         {/* search */}
+        <div>
+            <Search/>
+        </div>
         {/* search */}
         {/* login and my cart */}
+        <div>login and my cart</div>
         {/* login and my cart */}
       </div>
     </header>
---
+++ client/src/component/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200 '>
+      search
+    </div>
+  )
+}
+
+export default Search
