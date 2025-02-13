231be8e5152c56c4d091c9eccab0263e47561054
shahriar2880 <shahriarreza436@gmail.com>
2025-02-14 04:11:19 +0600

    complete the mobile version

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,69 +1,71 @@
 import React from "react";
 import logo from "../assets/big-bazar.png";
-import '../index.css'
+import "../index.css";
 import Search from "./Search";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { Link, useLocation } from "react-router-dom";
 import useMobile from "../hooks/useMobile";
 
 const Header = () => {
-    const [isMobile] = useMobile()
-    const location = useLocation()
-    const isSearchPage = location.pathname === "/search"
+  const [isMobile] = useMobile();
+  const location = useLocation();
+  const isSearchPage = location.pathname === "/search";
 
-    // const handleMobileUser = ()=>{
-    //     if(!user._id){
-    //         navigate("/login")
-    //         return
-    //     }
+  // const handleMobileUser = ()=>{
+  //     if(!user._id){
+  //         navigate("/login")
+  //         return
+  //     }
 
-    //     navigate("/user")
-    // }
+  //     navigate("/user")
+  // }
   return (
     //header tag use for seo frendly
-    <header className="h-30 min-w-[1px] py-2 lg:h-22 lg:shadow-md sticky top-0 bg-red-500 flex flex-col justify-center">
-      {
-        
-      }
-      <div className="container mx-auto flex items-center h-full flexcol justify-between">
-        {/* logo */}
-        <div className="h-full">
-          <Link to={"/"} className="h-full flex justify-center items-center">
-            <img
-              src={logo}
-              alt=""
-              height={50}
-              width={80}
-              className="hidden lg:block"
-            />
-            <img
-              src={logo}
-              alt=""
-              height={50}
-              width={80}
-              className="lg:hidden"
-            />
-          </Link>
+    <header className="h-30 min-w-[1px] py-2 lg:h-22 lg:shadow-md sticky top-0 bg-rose-700 flex flex-col justify-center">
+      {!(isSearchPage && isMobile) && (
+        <div className="container mx-auto flex items-center h-full flexcol justify-between">
+          {/* logo */}
+          <div className="h-full">
+            <Link to={"/"} className="h-full flex justify-center items-center">
+              <img
+                src={logo}
+                alt=""
+                height={50}
+                width={80}
+                className="hidden lg:block"
+              />
+              <img
+                src={logo}
+                alt=""
+                height={50}
+                width={80}
+                className="lg:hidden"
+              />
+            </Link>
+          </div>
+          {/* logo */}
+          {/* search */}
+          <div className="hidden lg:block">
+            <Search />
+          </div>
+          {/* search */}
+          {/* login and my cart */}
+          <div>
+            <button
+              className="text-neutral-600 lg:hidden"
+              // onClick={handleMobileUser}
+            >
+              <FaRegCircleUser size={26} />
+            </button>
+            <div className="hidden lg:block">login and my card</div>
+          </div>
+          {/* login and my cart */}
         </div>
-        {/* logo */}
-        {/* search */}
-        <div className="hidden lg:block">
-          <Search />
-        </div>
-        {/* search */}
-        {/* login and my cart */}
-        <div>
-          <button
-            className="text-neutral-600 lg:hidden"
-            // onClick={handleMobileUser}
-          >
-            <FaRegCircleUser size={26} />
-          </button>
-          <div className="hidden lg:block">login and my card</div>
-        </div>
-        {/* login and my cart */}
+      )}
+
+      <div className="container mx-auto px-2 lg:hidden search">
+        <Search />
       </div>
-      <div className="container mx-auto px-2 lg:hidden search"><Search/></div>
     </header>
   );
 };
--- client/src/component/Search.jsx
+++ client/src/component/Search.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { FaArrowLeft } from "react-icons/fa";
 import { TypeAnimation } from 'react-type-animation';
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useMobile from "../hooks/useMobile";
 
 
 const Search = () => {
     const navigate = useNavigate()
     const location = useLocation()
     const [isSearchPage,setIsSearchPage] = useState(false)
+    const [ isMobile ] = useMobile()
     const params = useLocation()
     const searchText = params.search.slice(3)
 
@@ -21,12 +23,29 @@ const Search = () => {
     const redirectToSearchPage =()=>{
         navigate("/search")
     }
+
+    const handleOnChange = (e)=>{
+        const value = e.target.value
+        const url = `/search?q=${value}`
+        navigate(url)
+    }
+
   return (
-    <div className="msx-sm:w-full w-full  min-w-[1px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200 ">
-      <button className="flex justify-center items-center h-full p-3 group-focus-within:text-primary-200">
-        <IoSearch size={20} />
-      </button>
-      <div className='w-full h-full'>
+    <div className='w-full  min-w-[300px] lg:min-w-[420px] h-11 lg:h-12 rounded-lg border overflow-hidden flex items-center text-neutral-500 bg-slate-50 group focus-within:border-primary-200 '>
+        <div>
+            {
+                (isMobile && isSearchPage ) ? (
+                    <Link to={"/"} className='flex justify-center items-center h-full p-2 m-1 group-focus-within:text-primary-200 bg-white rounded-full shadow-md'>
+                        <FaArrowLeft size={20}/>
+                    </Link>
+                ) :(
+                    <button className='flex justify-center items-center h-full p-3 group-focus-within:text-primary-200'>
+                        <IoSearch size={22}/>
+                    </button>
+                )
+            }
+        </div>
+        <div className='w-full h-full'>
             {
                 !isSearchPage ? (
                      //not in search page
@@ -62,15 +81,17 @@ const Search = () => {
                     <div className='w-full h-full'>
                         <input
                             type='text'
-                            placeholder='Search for atta, dal and more.'
+                            placeholder='Search for atta dal and more.'
                             autoFocus
                             defaultValue={searchText}
                             className='bg-transparent w-full h-full outline-none'
+                            onChange={handleOnChange}
                         />
                     </div>
                 )
             }
         </div>
+        
     </div>
   );
 };
