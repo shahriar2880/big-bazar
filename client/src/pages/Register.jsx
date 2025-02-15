6b84f698ad4c01dcf413408c9f01fe4e6b6a5740
shahriar2880 <shahriarreza436@gmail.com>
2025-02-16 01:19:44 +0600

    create email field in register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,56 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
   const [data, setData] = useState({
     name: "",
     email: "",
     password: "",
-    confirmPassword: ""
-})
+    confirmPassword: "",
+  });
 
-const handleChange = (e) => {
-  const { name, value } = e.target
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  setData((preve) => {
+    setData((preve) => {
       return {
-          ...preve,
-          [name]: value
-      }
-  })
-}
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
 
-const handleSubmit = async(e)=>{
-  e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  if(data.password !== data.confirmPassword){
-      toast.error(
-          "password and confirm password must be same"
-      )
-      return
-  }
-}
+    if (data.password !== data.confirmPassword) {
+      toast.error("password and confirm password must be same");
+      return;
+    }
+  };
   return (
-    <section className='w-full container mx-auto px-2'>
-      <div className='bg-white my-4 w-full max-w-lg mx-auto rounded p-7'>
+    <section className="w-full container mx-auto px-2">
+      <div className="bg-white my-4 w-full max-w-lg mx-auto rounded p-7">
         <p>Welcome to Big Bazar</p>
-        <form className='grid gap-4 mt-6' onSubmit={handleSubmit}>
-        <div className='grid gap-1'>
-                        <label htmlFor='name'>Name :</label>
-                        <input
-                            type='text'
-                            id='name'
-                            autoFocus
-                            className='bg-blue-50 p-2 border rounded outline-none focus:border-primary-200'
-                            name='name'
-                            value={data.name}
-                            onChange={handleChange}
-                            placeholder='Enter your name'
-                        />
-                    </div>
+        <form className="grid gap-4 mt-6" onSubmit={handleSubmit}>
+          <div className="grid gap-1">
+            <label htmlFor="name">Name :</label>
+            <input
+              type="text"
+              id="name"
+              autoFocus
+              className="bg-blue-50 p-2 border rounded outline-none focus:border-primary-200"
+              name="name"
+              value={data.name}
+              onChange={handleChange}
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="email">Email :</label>
+            <input
+              type="email"
+              id="email"
+              className="bg-blue-50 p-2 border rounded outline-none focus:border-primary-200"
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+              placeholder="Enter your email"
+            />
+          </div>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
