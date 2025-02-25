6ffb9d0a733d63226e15b56ba0577ed961628ccd
shahriar2880 <shahriarreza436@gmail.com>
2025-02-25 22:54:54 +0600

    extend the life span of access token with the help of refreshtoken

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -22,5 +22,33 @@ Axios.interceptors.request.use(
     }
 )
 
+//extend the life span of access token with the help of refreshtoken
+Axios.interceptors.request.use(
+    (response)=>{
+        return response
+    },
+    async(error)=>{
+        let originRequest = error.config 
+
+        if(error.response.status === 401 && !originRequest.retry){
+            originRequest.retry = true
+
+            const refreshToken = localStorage.getItem("refreshToken")
+
+            if(refreshToken){
+                const newAccessToken = await refreshAccessToken(refreshToken)
+
+                if(newAccessToken){
+                    originRequest.headers.Authorization = `Bearer ${newAccessToken}`
+                    return Axios(originRequest)
+                }
+            }
+        }
+        
+        return Promise.reject(error)
+    }
+)
+
+
 
 export default Axios
