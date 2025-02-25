190cd457aaca5c18f94882a71265de13d73d6a00
shahriar2880 <shahriarreza436@gmail.com>
2025-02-25 22:27:43 +0600

    sending access token in the header

--- client/src/utils/Axios.jsx
+++ client/src/utils/Axios.jsx
@@ -6,5 +6,21 @@ const Axios = axios.create({
     withCredentials : true
 })
 
+//sending access token in the header
+Axios.interceptors.request.use(
+    async(config)=>{
+        const accessToken = localStorage.getItem('accesstoken')
+
+        if(accessToken){
+            config.headers.Authorization = `Bearer ${accessToken}`
+        }
+
+        return config
+    },
+    (error)=>{
+        return Promise.reject(error)
+    }
+)
+
 
 export default Axios
