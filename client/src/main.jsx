23207675d47a4f15a5677534e27c10ba35ea7509
shahriar2880 <shahriarreza436@gmail.com>
2025-02-26 16:59:12 +0600

    initial value of user

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,12 @@ import router from './route/index'
 import { Provider } from 'react-redux'
 import App from './App'
 import { StrictMode } from 'react'
-// import { store } from './store/store.js'
+import { store } from './store/store.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router}/>
+  </Provider>
   // </StrictMode>,
 )
---
+++ client/src/store/userSlice.jsx
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialValue = {
+    _id : "",
+    name : "",
+    email : "",
+    avatar : "",
+    mobile : "",
+    verify_email : "",
+    last_login_date : "",
+    status : "",
+    address_details : [],
+    shopping_cart : [],
+    orderHistory : [],
+    role : "",
+}
+
+const userSlice = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default userSlice
