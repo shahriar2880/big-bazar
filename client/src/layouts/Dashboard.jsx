bb7bc350b9798dbfcf6cbe37c3f0b9fd964d8ea1
shahriar2880 <shahriarreza436@gmail.com>
2025-03-20 19:25:39 +0600

    left and right menu

--- client/src/layouts/Dashboard.jsx
+++ client/src/layouts/Dashboard.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import UserMenu from '../component/UserMenu'
+import { Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
+    <section className='bg-white'>
+        <div className='container mx-auto p-3 grid lg:grid-cols-[250px,1fr]  '>
+                {/**left for menu */}
+                <div className='py-4 sticky top-24 max-h-[calc(100vh-96px)] overflow-y-auto hidden lg:block border-r'>
+                    <UserMenu/>
+                </div>
+
+
+                {/**right for content */}
+                <div className='bg-white min-h-[75vh] '>
+                    <Outlet/>
+                </div>
+        </div>
+    </section>
   )
 }
 
