0a1107a3f1017b0845a01eaeeea318313e37cb56
shahriar2880 <shahriarreza436@gmail.com>
2025-04-14 03:30:02 +0600

    profile upload and display image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { FaRegUserCircle } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
 
 const Profile = () => {
   const user = useSelector((state) => state.user)
+  console.log("user from store", user)
   return (
     <div>
-      profile
+      {/**----profile upload and display image---*/}
+      <div className='w-20 h-20 bg-red-500 flex items-center justify-center rounded-full overflow-hidden drop-shadow-sm'>
+            {
+                user.avatar ? (
+                    <img 
+                      alt={user.name}
+                      src={user.avatar}
+                      className='w-full h-full'
+                    />
+                ) : (
+                    <FaRegUserCircle size={65}/>
+                )
+            }
+        </div>
     </div>
   )
 }
