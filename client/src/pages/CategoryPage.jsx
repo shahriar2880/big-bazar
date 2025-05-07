e98a3eaaf0d133a8db7b29ce7e1f90ac21c78a7c
shahriar2880 <shahriarreza436@gmail.com>
2025-05-08 03:32:11 +0600

    create  a category button

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -5,6 +5,7 @@ const CategoryPage = () => {
     <section>
       <div className="p-2 bg-white shadow-md flex items-center justify-between">
         <h2 className="font-semibold">Category</h2>
+        <button className="text-sm border border-x-primary-200 hover:bg-primary-200 px-3 py-1 rounded">Add Category</button>
       </div>
     </section>
   );
